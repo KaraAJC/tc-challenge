eb2d6dc89760feea6cc14f23cecf20c9d423bdd5
karaAJC <KaraAJC@gmail.com>
2016-09-28 16:24:45 -0500

    updates code with observation and failing test for sort

--- jasmine/spec/assignment_1Spec.js
+++ jasmine/spec/assignment_1Spec.js
@@ -6,8 +6,8 @@ describe("sortBiggestDiff", function() {
   it("can handle negative numbers in an array", function(){
     expect(sortBiggestDiff([1,9,3,-4])).toEqual(13);
   });
-  it("can handle an enormous array", function(){
-    expect(sortBiggestDiff([1,9,3,-4])).toEqual(13);
+  it("can handle a really random array", function(){
+    expect(sortBiggestDiff([3, 2, 1, 5, 3, 7, 4, 7, 9, 5, -2, 0, -23, 44334, 3])).toEqual(44357);
   });
 });
 
@@ -16,6 +16,9 @@ describe("mathBiggestDiff", function() {
     expect(mathBiggestDiff([1,9,3])).toEqual(8);
   });
   it("can also handle negative numbers in an array", function(){
-    expect(sortBiggestDiff([1,9,3,-4])).toEqual(13);
+    expect(mathBiggestDiff([1,9,3,-4])).toEqual(13);
+  });
+  it("can also handle a really random array", function(){
+    expect(mathBiggestDiff([3, 2, 1, 5, 3, 7, 4, 7, 9, 5, -2, 0, -23, 44334, 3])).toEqual(44357);
   });
 });
